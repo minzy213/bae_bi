2d3dde5c5aa29268a496237c4c06f6e832b615d6
SKKU-03\User <inhwancho02@skkukdp.re.kr>
2023-10-11 18:03:25 +0900

    [bae-bi] 조인환
    main 페이지 데모 완성 다음 깊이의 html 만들어야 합니다. ( main/category_name )

--- bae_bi/board/views.py
+++ bae_bi/board/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'board/main.html')
+
+    dict_category = [
+        {'name':'치킨', 'url':'chicken', 'path':"https://nenechicken.com/17_new/images/menu/30005.jpg"},
+        {'name':'한식', 'url':'korean', 'path':"http://www.hotelrestaurant.co.kr/data/photos/20180205/art_15175330519518_43b250.bmp"},
+    ]
+    return render(request, 'board/main.html', {"dict_category":dict_category} )
 
 def category(request):
     pass
