5363f9db9ebe55d12debb66c1b0e31de591da716
mindanjichu <a09999@skkukdp.re.kr>
2023-10-12 16:25:59 +0900

    [강민지]
    수정내용:
    def category(request):
        pass
    
    추가

--- bae_bi/board/views.py
+++ bae_bi/board/views.py
@@ -19,10 +19,17 @@ def main(request):  # 수정한 코드
 
     return render(request, 'board/main.html', {'category': category})
 
+
+def category(request):
+    pass
+
+
     # dict_category = [
     #     {'name': '치킨', 'url': 'chicken',
     #         'path': "https://nenechicken.com/17_new/images/menu/30005.jpg"},
     #     {'name': '한식', 'url': 'korean',
     #         'path': "http://www.hotelrestaurant.co.kr/data/photos/20180205/art_15175330519518_43b250.bmp"},
     # ]
+
     # return render(request, 'board/main.html', {'category': category})
+
