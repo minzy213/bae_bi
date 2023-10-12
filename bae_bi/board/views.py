28f92a5c442f893d5973a27541e2ee4e673a0b32
codernoah404 <inhwancho02@gmail.com>
2023-10-13 01:07:55 +0900

    [bae-bi] 조인환
    merge 과정에서 누락된 def main(request): 추가합니다. 주석처리도 다시 진행했습니다.

--- bae_bi/board/views.py
+++ bae_bi/board/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import Category
 # Create your views here.
-
+def main(request):
     category = [
         {'name':'치킨', 'url':'chicken', 'path':"https://nenechicken.com/17_new/images/menu/30005.jpg"},
         {'name':'한식', 'url':'korean', 'path':"http://www.hotelrestaurant.co.kr/data/photos/20180205/art_15175330519518_43b250.bmp"},
@@ -10,11 +10,11 @@ from .models import Category
     return render(request, 'board/main.html', {"category":category} )
 
 def category(request, category_name):
-    #카테고리에 맞는 데이터를 받아와서 넘겨줘야 합니다.
-    cat_list = {
-        'name': '치킨', 'store_list' : [
-            {'name':'BHC 동판교점','company':'요기요' ,'coupon': '요기요 5% 할인'},
-            {'name':'교촌치킨 동판교점','company':'배민', 'coupon': '배민 2,000원 할인'},
-        ]
-    }
-    return render(request, 'board/category.html' , {'cat_list':cat_list})
+    pass
+    # cat_list = {
+    #     'name': '치킨', 'store_list' : [
+    #         {'name':'BHC 동판교점','company':'요기요' ,'coupon': '요기요 5% 할인'},
+    #         {'name':'교촌치킨 동판교점','company':'배민', 'coupon': '배민 2,000원 할인'},
+    #     ]
+    # }
+    # return render(request, 'board/category.html' , {'cat_list':cat_list})
