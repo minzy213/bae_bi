1d006708b0d298d1992cf9cb3661187d5691656b
SKKU-03\User <inhwancho02@skkukdp.re.kr>
2023-10-12 18:24:36 +0900

    [bae-bi] 조인환
    깊이 들어간 페이지 구현중입니다.

--- bae_bi/board/views.py
+++ bae_bi/board/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
 
-    dict_category = [
+    category = [
         {'name':'치킨', 'url':'chicken', 'path':"https://nenechicken.com/17_new/images/menu/30005.jpg"},
         {'name':'한식', 'url':'korean', 'path':"http://www.hotelrestaurant.co.kr/data/photos/20180205/art_15175330519518_43b250.bmp"},
     ]
-    return render(request, 'board/main.html', {"dict_category":dict_category} )
+    return render(request, 'board/main.html', {"category":category} )
 
-def category(request):
-    pass
+def category(request, category_name):
+    #카테고리에 맞는 데이터를 받아와서 넘겨줘야 합니다.
+    
+    return render(request, 'board/category.html' )
