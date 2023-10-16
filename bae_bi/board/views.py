6645c1da5ef22d9fb4c357fa88581ed7a2b61c17
codernoah404 <inhwancho02@gmail.com>
2023-10-17 01:27:23 +0900

    [bae-bi] 조인환
    전체 페이지의 폰트를 바꾸고 카테고리 폰트만 다르게 구현했습니다. 카테고리의 경우 10월 17일 상의가 필요합니다.
    디테일 페이지의 디테일이 수정되었습니다.
    카테고리의 배경 이미지를 수정해야합니다.

--- bae_bi/board/views.py
+++ bae_bi/board/views.py
@@ -47,25 +47,22 @@ def update(request):
     # bhc를 눌렀다는 가정하에...
     if request.method == "POST":
         menu = {
-            'store' : 'BHC', 
-            'dil_time':'배민: 40분 | 요기요: 50분',
-        'menus':
-            [{'name':'뿌링클', 
-            'coupon':[
-            {'company':'요기요', 'coupon': '5% 할인'},
-            {'company':'배달의 민족', 'coupon': '5% 할인'},
-            {'company':'쿠팡이츠', 'coupon': '5% 할인'},]},
-            {'name':'치킨 1', 
-         'coupon':[
-            {'company':'요기요', 'coupon': '5% 할인'},
-            {'company':'배달의 민족', 'coupon': '5% 할인'},
-            {'company':'쿠팡이츠', 'coupon': '5% 할인'},]},
-        {'name':'치킨 2', 
-         'coupon':[
-            {'company':'요기요', 'coupon': '5% 할인'},
-            {'company':'배달의 민족', 'coupon': '5% 할인'},
-            {'company':'쿠팡이츠', 'coupon': '5% 할인'},]}]
-        }   
+            'name': 'BHC 동판교점', 
+            'service': ['배민', '요기요', '쿠팡이츠'],
+            'time': ['','45-55분', '53-55분'],
+            'fee': ['2,300원','', '2,500원'],
+            'coupon': ['1,000원 할인', '',''],
+            'menu_list':[
+                {'name':'뿌링클', 'price':'18,000', 'info':'뿌링뿌링, 세상에 없던 마법의 맛 뿌링클', 'thumbnail_path':'path', 'coupon':'| 요기요: 5% 할인 | 배민: 2,000원 할인 |'},
+                {'name':'마법클', 'price':'17,000', 'info':'마늘, 버터와 크런치한 후레이크의 마법같은 조합', 'thumbnail_path':'path', 'coupon':''},],
+            'review_list':[
+                { 'platform':0 , 'author':'mi**', 'content':'맛있어요', 'rate':3, 'img_path':'http://www.bhc.co.kr/upload/bhc/menu/%EB%BF%8C%EB%A7%81%ED%81%B4_410x271.png', 'created_at':'2023.10.12 16:00', 'menu':'뿌링클 콤보'},
+                { 'platform':1, 'author':'nina**', 'content':'진짜 맛있음 bb', 'rate':4, 'img_path':'http://www.bhc.co.kr/upload/bhc/menu/%ED%95%AB%ED%9B%84%EB%9D%BC%EC%9D%B4%EB%93%9C-%EC%BD%A4%EB%B3%B4_410x271.png', 'created_at':'2023.9.12 17:00', 'menu':'후라이드 콤보'},
+                { 'platform':1, 'author':'nina**', 'content':'진짜 맛있음 bb', 'rate':4, 'img_path':'http://www.bhc.co.kr/upload/bhc/menu/%ED%95%AB%ED%9B%84%EB%9D%BC%EC%9D%B4%EB%93%9C-%EC%BD%A4%EB%B3%B4_410x271.png', 'created_at':'2023.9.12 17:00', 'menu':'후라이드 콤보'},
+                { 'platform':1, 'author':'nina**', 'content':'진짜 맛있음 bb', 'rate':4, 'img_path':'http://www.bhc.co.kr/upload/bhc/menu/%ED%95%AB%ED%9B%84%EB%9D%BC%EC%9D%B4%EB%93%9C-%EC%BD%A4%EB%B3%B4_410x271.png', 'created_at':'2023.9.12 17:00', 'menu':'후라이드 콤보'},
+                { 'platform':2, 'author':'nono**', 'content':'진짜 맛있어요 강추!', 'rate':5, 'img_path':'http://www.bhc.co.kr/upload/bhc/menu/%ED%95%AB%ED%9B%84%EB%9D%BC%EC%9D%B4%EB%93%9C-%EC%BD%A4%EB%B3%B4_410x271.png', 'created_at':'2023.11.12 17:00', 'menu':'써브웨이 클럽 （15cm세트）/1(빵선택(허니오트),치즈선택(모차렐라치즈),빵／미'},]
+            }
+
         return HttpResponse(json.dumps(menu))
 
     return HttpResponse({"error": "invalid request"})
